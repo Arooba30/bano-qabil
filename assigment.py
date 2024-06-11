0ac9620f391fac4f16776c0bb8fe28de77ede967
AROOBA30 <irfannoreen388@gmail.com>
2024-06-11 19:45:12 +0500

    Add new paragraph

---
+++ assigment.py
@@ -0,0 +1,6 @@
+def area_of_the_triangle(base,height):return 0.5 * base * height
+base_of_traingle=10
+height_of_traingle=5
+area_of_the_triangle(base_of_traingle,height_of_traingle)
+print("area of the traingle:{triangle_area}square units")
+  
